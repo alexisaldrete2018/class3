f5c338d1a6cdc5c225c15bf096b66f7d7db43951
alexisaldrete2018 <40009553+alexisaldrete2018@users.noreply.github.com>
2022-04-06 18:52:50 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function greet(name){
+    return "Hello "+ name +"!";
+}
+
+console.log(greet("Alexis"));
+
+let greet2 = function(name){
+    return "Hello "+ name +"!";
+}
+
+console.log(greet2("Alexis"))
